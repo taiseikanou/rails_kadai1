c6f0cd1d403878fbddfe4a334a67d9e185cd05a6
加納大成 <kanoutaisei@kanoutaiseinoMacBook-Air.local>
2024-03-20 19:07:01 +0900

    新規作成機能の作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,12 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(params.require(:post).permit(:title, :start_date, :end_date, :all_day, :memo))
+      if @post.save
+        redirect_to :posts
+        else
+        render "new"
+      end
   end
 
   def show
